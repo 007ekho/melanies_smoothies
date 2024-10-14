582addaa82303351f48793ef66524445520a4a35
suehi <110624980+007ekho@users.noreply.github.com>
2024-10-14 19:09:05 +0100

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -53,8 +53,9 @@ if ingredients_list:
     st.write(my_insert_stmt)
 
     time_to_insert = st.button("submit Order")
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-    st.text(fruityvice_response)
+    # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+    # fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width= True)
+    # st.text(fv_df)
 
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
